5f337d5582ee12fea22c66083661110713515d06
TambroFrancisca <queenmaskot0285@gmail.com>
2020-08-07 12:23:23 +0000

    commit

---
+++ python/python.py
@@ -0,0 +1,18 @@
+print("Tambro Francisca")
+
+fullname = "Tambro Francisca"
+age = 25
+married = True
+
+hobbies = ['singing', 'cooking', 'dancing', 'exploring']
+
+kids = {
+    "Noble": "male"
+    "Otima": "female"
+    "Okleki": "female"
+}
+
+print(hobbies)
+
+if(married== True):
+    print("happy marriage")
